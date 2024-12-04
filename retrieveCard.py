7cd15cfc8299191f7f7fec3ef83eccfcabc892cc
Tom <Tom@DESKTOP-30VCA5G>
2024-12-04 13:24:22 -0500

    demonstrates we can use wildcard in searches

--- retrieveCard.py
+++ retrieveCard.py
@@ -7,4 +7,7 @@ from mtgsdk import Changelog
 
 bolts = Card.where(name="Lightning Bolt").all()
 
+# NOTE: This demonstrates that we can use wildcard characters to make a search, it doesn't need to be an exact name
+uro = Card.where(name="Uro%Titan%").all()
+
 secondStopper="fasdfasdf"
